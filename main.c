1f2d135df8ef2f7e23fce8983b8608923dbc7ebc
Umair <omerjerk@gmail.com>
2018-09-09 14:12:09 -0400

    add main function including file i/o

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /*    
 codebuffer is a valid pointer to 8080 assembly code    
 pc is the current offset into the code
@@ -28,4 +31,31 @@ int disassemble8080Op(unsigned char *codebuffer, int pc) {
     printf("\n");    
 
     return opbytes;    
-}    
+}
+
+int main (int argc, char**argv) {    
+    FILE *f= fopen(argv[1], "rb");    
+    
+    if (f == NULL) {
+        printf("error: Couldn't open %s\n", argv[1]);
+        exit(1);
+    }
+
+    //Get the file size and read it into a memory buffer    
+    fseek(f, 0L, SEEK_END);    
+    int fsize = ftell(f);    
+    fseek(f, 0L, SEEK_SET);    
+
+    unsigned char *buffer = malloc(fsize);    
+
+    fread(buffer, fsize, 1, f);    
+    fclose(f);    
+
+    int pc = 0;    
+
+    while (pc < fsize) {    
+        pc += disassemble8080Op(buffer, pc);    
+    }
+
+    return 0;    
+}
