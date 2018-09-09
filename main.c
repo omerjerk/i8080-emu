eec18571148659b2fcbc46df39bd87ba8ece9b6d
Umair <omerjerk@gmail.com>
2018-09-09 14:07:59 -0400

    initial disassemble function
    Signed-off-by: Umair <omerjerk@gmail.com>

---
+++ main.c
@@ -0,0 +1,31 @@
+/*    
+codebuffer is a valid pointer to 8080 assembly code    
+pc is the current offset into the code
+returns the number of bytes of the op    
+*/
+int disassemble8080Op(unsigned char *codebuffer, int pc) {
+    unsigned char *code = &codebuffer[pc];    
+    int opbytes = 1;    
+    printf ("%04x ", pc);    
+    switch (*code)    
+    {    
+        case 0x00: printf("NOP"); break;    
+        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
+        case 0x02: printf("STAX   B"); break;    
+        case 0x03: printf("INX    B"); break;    
+        case 0x04: printf("INR    B"); break;    
+        case 0x05: printf("DCR    B"); break;    
+        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
+        case 0x07: printf("RLC"); break;    
+        case 0x08: printf("NOP"); break;    
+        /* ........ */    
+        case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;    
+        /* ........ */    
+        case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;    
+        /* ........ */    
+    }    
+
+    printf("\n");    
+
+    return opbytes;    
+}    
