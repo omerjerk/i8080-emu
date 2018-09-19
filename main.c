3e6fc1905161107f83dd083c21ca5e4ea4e5d3d8
Umair <omerjerk@gmail.com>
2018-09-19 00:59:59 -0400

    structs to represent CPU state
    Signed-off-by: Umair <omerjerk@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+
+typedef struct ConditionCodes {
+    uint8_t    z:1;
+    uint8_t    s:1;
+    uint8_t    p:1;
+    uint8_t    cy:1;
+    uint8_t    ac:1;
+    uint8_t    pad:3;
+} ConditionCodes;
+
+typedef struct State8080 {
+    uint8_t    a;
+    uint8_t    b;
+    uint8_t    c;
+    uint8_t    d;
+    uint8_t    e;
+    uint8_t    h;
+    uint8_t    l;
+    uint16_t   sp;
+    uint16_t   pc;
+    uint8_t    *memory;
+    struct     ConditionCodes      cc;
+    uint8_t    int_enable;
+} State8080;
 
 /*    
 codebuffer is a valid pointer to 8080 assembly code    
