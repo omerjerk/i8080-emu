eaf567d79357f59ad1c5d6da1e374b9bc81b2743
Umair <omerjerk@gmail.com>
2018-11-14 00:26:39 -0500

    fix orientation of the texture
    Signed-off-by: Umair <omerjerk@gmail.com>

--- main.c
+++ main.c
@@ -96,9 +96,9 @@ initGL(GtkWidget* glArea, gpointer data) {
     float vertices[] = {
         // positions         // texture coords
         1.0f,  1.0f, 0.0f,   1.0f, 1.0f,   // top right
-        1.0f, -1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        1.0f, -1.0f, 0.0f,   0.0f, 1.0f,   // bottom right
        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,   // bottom left
-       -1.0f,  1.0f, 0.0f,   0.0f, 1.0f    // top left 
+       -1.0f,  1.0f, 0.0f,   1.0f, 0.0f    // top left
     };
 
     unsigned int indices[] = {  // note that we start from 0!
